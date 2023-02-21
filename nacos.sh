53e26a7ba99b6bbc755296650c089ec1e59f8def
zgbjty <371292290@qq.com>
2023-02-21 11:47:28 +0800

    update

--- nacos.sh
+++ nacos.sh
@@ -7,28 +7,28 @@ TAG=v2.2.0-slim
 
 # Check if the network exists
 if docker network ls --format '{{.Name}}'| grep $NETWORK > /dev/null 2>&1; then
-  echo "Network $NETWORK already exists , skip network create"
+  echo "Network $NETWORK already exists,skip network create"
 else
   docker network create $NETWORK
 fi
 
 # docker pull nacos server image
 if docker images --format '{{.Repository}}:{{.Tag}}'| grep $IMAGE:$TAG > /dev/null 2>&1; then
-  echo "Image $IMAGE:$TAG already exists , skip image pull"
+  echo "Image $IMAGE:$TAG already exists,skip image pull"
   else
     docker pull $IMAGE:$TAG
 fi
 
 # docker create volume
 if docker volume ls --format '{{.Name}}'|grep $VOLUME > /dev/null 2>&1; then
-   echo "Volume $VOLUME already exists , skip volume create"
+   echo "Volume $VOLUME already exists,skip volume create"
    else
      docker volume create $VOLUME
 fi
 
 # docker start container
 if docker ps --format '{{.Image}}'|grep $IMAGE:$TAG > /dev/null 2>&1; then
-  echo "Image $IMAGE:$TAG already in use , skip docker start conatiner : $CONTAINER"
+  echo "Image $IMAGE:$TAG already in use,container name : $(docker ps --format '{{.Image}} {{.Names}}'|grep $IMAGE:$TAG|awk '{print $2}')"
   else
     if docker container ls -a --format '{{.Image}}' | grep $IMAGE:$TAG > /dev/null 2>&1; then
        container_name=$(docker container ls -a --format '{{.Image}} {{.Names}}'|grep $IMAGE:$TAG|awk '{print $2}')
