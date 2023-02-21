1b0eaa5720db4d7435425a5789c27ca45b4d9688
zgbjty <371292290@qq.com>
2023-02-21 12:08:00 +0800

    update file

--- nacos-startup.sh
+++ nacos-startup.sh
@@ -15,28 +15,28 @@ ENV_FILE=nacos-startup.env
 # docker container listen on local machine port
 LISTEN_PORT=8848
 
-# Check if the network exists
+# Network
 if docker network ls --format '{{.Name}}'| grep $NETWORK > /dev/null 2>&1; then
   echo "Network $NETWORK already exists,skip network create"
 else
   docker network create $NETWORK
 fi
 
-# docker pull nacos server image
+# Image
 if docker images --format '{{.Repository}}:{{.Tag}}'| grep $IMAGE:$TAG > /dev/null 2>&1; then
   echo "Image $IMAGE:$TAG already exists,skip image pull"
   else
     docker pull $IMAGE:$TAG
 fi
 
-# docker create volume
+# Volume
 if docker volume ls --format '{{.Name}}'|grep $VOLUME > /dev/null 2>&1; then
    echo "Volume $VOLUME already exists,skip volume create"
    else
      docker volume create $VOLUME
 fi
 
-# docker start container
+# Container
 if docker ps --format '{{.Image}}'|grep $IMAGE:$TAG > /dev/null 2>&1; then
   echo "Image $IMAGE:$TAG already in use,container name : $(docker ps --format '{{.Image}} {{.Names}}'|grep $IMAGE:$TAG|awk '{print $2}')"
   else
