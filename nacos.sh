d7a1d280c1d92ac9f1f210e6b22a85cd1be68508
zgbjty <371292290@qq.com>
2023-02-21 11:02:03 +0800

    update file

--- nacos.sh
+++ nacos.sh
@@ -6,31 +6,29 @@ IMAGE=nacos/nacos-server
 TAG=v2.2.0-slim
 
 # Check if the network exists
-if ! docker network inspect $NETWORK > /dev/null 2>&1; then
-  # Create the network if it doesn't exist
-  echo "Creating network $NETWORK"
-  docker network create $NETWORK
-else
+if docker network ls --format '{{.Name}}'| grep $NETWORK; then
   echo "Network $NETWORK already exists , skip network create"
+else
+  docker network create $NETWORK
 fi
 
 # docker pull nacos server image
 if docker images --format '{{.Repository}}:{{.Tag}}'| grep $IMAGE:$TAG; then
-  echo "$IMAGE:$TAG already exists , skip image pull"
+  echo "Image $IMAGE:$TAG already exists , skip image pull"
   else
     docker pull $IMAGE:$TAG
 fi
 
 # docker create volume
 if docker volume ls --format '{{.Name}}'|grep $VOLUME; then
-   echo "docker volume name $VOLUME already exist , skip volume create"
+   echo "Volume $VOLUME already exists , skip volume create"
    else
      docker volume create $VOLUME
 fi
 
 # docker start container
 if docker ps --format '{{.Image}}'|grep $IMAGE:TAG; then
-  echo "$IMAGE:$TAG already in use , skip docker start $CONTAINER"
+  echo "Image $IMAGE:$TAG already in use , skip docker start conatiner : $CONTAINER"
   else
     docker run --name $CONTAINER --network $NETWORK \ 
     --env-file nacos.env \
