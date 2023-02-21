e81bd74b70722c6a7bd3cb3290c16c38242bc6af
zgbjty <371292290@qq.com>
2023-02-21 14:04:40 +0800

    Add regisry start up shell script

---
+++ registry-startup.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# docker conatiner name
+CONTAINER=registry
+# docker network for current container
+NETWORK=localhost
+# docker volume for container persist data
+VOLUME=registry-volume
+# docker container image
+IMAGE=registry
+# docker container image tag
+TAG=2
+# docker container env file
+ENV_FILE=registry-startup.env
+# docker container listen on local machine port
+PORT=5000
+
+
+# Network
+if docker network ls --format '{{.Name}}'| grep $NETWORK > /dev/null 2>&1; then
+  echo "Network $NETWORK already exists,skip network create"
+else
+  docker network create $NETWORK
+fi
+
+# Image
+if docker images --format '{{.Repository}}:{{.Tag}}'| grep $IMAGE:$TAG > /dev/null 2>&1; then
+  echo "Image $IMAGE:$TAG already exists,skip image pull"
+  else
+    docker pull $IMAGE:$TAG
+fi
+
+# Volume
+if docker volume ls --format '{{.Name}}'|grep $VOLUME > /dev/null 2>&1; then
+   echo "Volume $VOLUME already exists,skip volume create"
+   else
+     docker volume create $VOLUME
+fi
+
+# Container
+if docker ps --format '{{.Image}}'|grep $IMAGE:$TAG > /dev/null 2>&1; then
+  echo "Image $IMAGE:$TAG already in use,container name : $(docker ps --format '{{.Image}} {{.Names}}'|grep $IMAGE:$TAG|awk '{print $2}')"
+  else
+    if docker container ls -a --format '{{.Image}}' | grep $IMAGE:$TAG > /dev/null 2>&1; then
+       container_name=$(docker container ls -a --format '{{.Image}} {{.Names}}'|grep $IMAGE:$TAG|awk '{print $2}')
+       echo "Start exited container $container_name"
+       docker start $container_name
+       else
+         echo "Start new container $CONTAINER"
+         docker run --name $CONTAINER --network $NETWORK --env-file=$ENV_FILE -v $VOLUME:/var/lib/registry -p $PORT:5000 -d $IMAGE:$TAG
+    fi
+fi
